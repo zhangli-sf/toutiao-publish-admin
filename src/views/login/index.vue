b924f500ab7d20aa7942d540553ea154b31718e4
zhangli-sf <67639308+zhangli-sf@users.noreply.github.com>
2020-11-30 10:00:56 +0800

    登录页面搭搭建

---
+++ src/views/login/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="login-container">
+      <el-form ref="form" :model="user" label-width="80px">
+  <el-form-item label="手机号">
+    <el-input v-model="user.mobile"></el-input>
+  </el-form-item>
+  <el-form-item label="验证码">
+    <el-input v-model="user.code"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="onSubmit">登录</el-button>
+  </el-form-item>
+</el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
+}
+</script>
+
+<style scoped lang=scss></style>
